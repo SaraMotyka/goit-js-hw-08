83a0ad3ad34076d33a09889ec8abe2470a440d10
SaraMotyka <saramotyka97@gmail.com>
2023-05-02 20:18:24 +0200

    cd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,46 @@
+import SimpleLightbox from '../../node_modules/simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryEls = createGalleryEls(galleryItems);
 
+function createGalleryEls(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+            <a href="${original}" class= "gallery__item">
+            <img 
+            class= "gallery__image"
+            src= "${preview}"
+            data-source= "${original}"
+            alt= "${description}"
+            />
+            </a>
+            </li>`;
+    })
+    .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', galleryEls);
+gallery.addEventListener('click', onGalleryClick);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+
+  document.addEventListener('keydown', ({ key }) => {
+    if (key === 'Escape') {
+      instance.close();
+    }
+  });
+}
 console.log(galleryItems);
-console.log('x');
-console.log('x');
