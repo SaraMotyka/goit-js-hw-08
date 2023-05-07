b9a996a3cfee924a9278f369420d990201e354d8
SaraMotyka <saramotyka97@gmail.com>
2023-05-07 22:03:30 +0200

    saved changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+try {
+  //1
+  const localStorageKey = 'videoplayer-current-time';
+  const iframe = document.querySelector('#vimeo-player');
+  const player = new Vimeo(iframe);
+  //2
+  const onPlay = function (data) {
+    localStorage.setItem(localStorageKey, data.seconds);
+  };
+  //3
+  player.setCurrentTime(JSON.parse(localStorage.getItem(localStorageKey)));
+  //4
+  player.on('timeupdate', throttle(onPlay, 1000));
+  console.log('Everything is fine!');
+} catch (error) {
+  console.log('Sorry, try later!');
+}
+
+//1 Dpodawanie zmiennych
+//2 Dodanie key i value do local storage
+//3 Odtwarzanie po przeładowaniu strony od ostatniego czasu
+//4 Aktualizacja czasu w local storage
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+const dataForm = {
+  email: '',
+  message: '',
+};
+const savedData = throttle(e => {
+  dataForm[e.target.name] = e.target.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(dataForm));
+}, 500);
+
+form.addEventListener('input', savedData);
+
+function refreshPage() {
+  const newInput = localStorage.getItem(localStorageKey);
+  const parsedNewInput = JSON.parse(newInput);
+  if (parsedNewInput) {
+    form.email.value = parsedNewInput.email;
+    form.message.value = parsedNewInput.message;
+  } else {
+    form.email.value = '';
+    form.message.value = '';
+  }
+}
+refreshPage();
+
+function onSubmit() {
+  localStorage.clear();
+}
+form.addEventListener('submit', onSubmit);
